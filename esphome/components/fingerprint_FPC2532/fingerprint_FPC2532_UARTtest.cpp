d53fb2a5ff0b1cce0c4d54a05453dd995e5e63b3
luigi-pi <pezzatoluigi.mail@gmail.com>
2025-10-25 15:49:44 +0200

    setting UART communication

--- esphome/components/fingerprint_FPC2532/fingerprint_FPC2532_UARTtest.cpp
+++ esphome/components/fingerprint_FPC2532/fingerprint_FPC2532_UARTtest.cpp
@@ -11,27 +11,36 @@ static const char *const TAG = "fingerprint_FPC2532";
 void FingerprintFPC2532Component::update() {}
 
 void FingerprintFPC2532Component::setup() {
-  fpc_result_t fpc_hal_init(void) { return FPC_RESULT_OK; }
-
-  this->get_parameters_();  // added
+  this->fpc_hal_init();
+  // this->get_parameters_();  // added
 }
 
-fpc_result_t FingerprintFPC2532Component::fpc_hal_tx(uint8_t *data, size_t len) {
+/*
+------------------------
+HAL FUNCTIONS DEFINITONS
+------------------------
+*/
+fpc_result_t FingerprintFPC2532Component::fpc_hal_init(void) { return FPC_RESULT_OK; }
+fpc_result_t FingerprintFPC2532Component::fpc_hal_tx(uint8_t *data, std::size_t len) {
   if (!data || len == 0) {
     return FPC_RESULT_FAILURE;
   }
-  while (this->available())
-    this->fpc_hal_rx();
+  // while (this->available()){
+  //  result = this->fpc_host_sample_handle_rx_data();
+  // if (result != FPC_RESULT_OK) {
+  //  ESP_LOGCONFIG(TAG, " Failed to handle RX data, error %d", result);
+  //  break;
+  // }
   this->write_array(data, len);
   return FPC_RESULT_OK;  // doesn't guarantee array was actually sent: no timeout handling here
 }
-fpc_result_t FingerprintFPC2532Component::fpc_hal_rx(uint8_t *data, size_t len) {
+fpc_result_t FingerprintFPC2532Component::fpc_hal_rx(uint8_t *data, std::size_t len) {
   int rc = 0;
   rc = !this->read_array(data, len);
 
   return rc == 0 ? FPC_RESULT_OK : FPC_RESULT_FAILURE;
 }
-
+void fpc_hal_delay_ms(uint32_t ms) { delay(ms); }
 void FingerprintFPC2532Component::dump_config() {}
 
 }  // namespace fingerprint_FPC2532
