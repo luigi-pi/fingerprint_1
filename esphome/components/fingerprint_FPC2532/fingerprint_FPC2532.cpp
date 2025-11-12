81b982f9dcc965a1d98eabf564ec0a0b7e49c830
luigi-pi <pezzatoluigi.mail@gmail.com>
2025-11-12 17:14:01 +0100

    g

--- esphome/components/fingerprint_FPC2532/fingerprint_FPC2532.cpp
+++ esphome/components/fingerprint_FPC2532/fingerprint_FPC2532.cpp
@@ -876,7 +876,8 @@ fpc::fpc_result_t FingerprintFPC2532Component::parse_cmd_enroll_status(fpc::fpc_
   }
 
   if (cmd_callbacks.on_enroll) {
-    cmd_callbacks.on_enroll(status->feedback, status->samples_remaining);
+    // cmd_callbacks.on_enroll(status->feedback, status->samples_remaining);
+    ESP_LOGI(TAG, "callback on ensollment chiamata");
   }
 
   return result;
