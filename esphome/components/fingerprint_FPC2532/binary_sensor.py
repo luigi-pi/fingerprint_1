ad52ac2c9bd122240f70f84a4c29175f28b40d50
luigi-pi <pezzatoluigi.mail@gmail.com>
2025-11-15 23:18:04 +0100

    f

--- esphome/components/fingerprint_FPC2532/binary_sensor.py
+++ esphome/components/fingerprint_FPC2532/binary_sensor.py
@@ -1,48 +1,50 @@
 import esphome.codegen as cg
 from esphome.components import binary_sensor
 import esphome.config_validation as cv
-from esphome.const import CONF_ICON, ICON_KEY_PLUS
+from esphome.const import ICON_KEY_PLUS
 
 from . import CONF_FINGERPRINT_FPC2532_ID, FingerprintFPC2532Component
 
 CONF_SET_STATUS_AT_BOOT = "status_at_boot"
 CONF_STOP_MODE_UART = "stop_mode_uart"
 CONF_UART_IRQ_BEFORE_TX = "uart_irq_before_tx"
+CONF_ENROLLING = "enrolling_binary"
 ICON_CONFIG = "mdi:settings"
 
 DEPENDENCIES = ["fingerprint_FPC2532"]
 
-CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend(
+CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_FINGERPRINT_FPC2532_ID): cv.use_id(
             FingerprintFPC2532Component
         ),
-        cv.Optional(CONF_ICON, default=ICON_KEY_PLUS): cv.icon,
-        cv.Optional(CONF_SET_STATUS_AT_BOOT, default=True): cv.boolean,
-        cv.Optional(CONF_STOP_MODE_UART, default=False): cv.boolean,
-        cv.Optional(CONF_UART_IRQ_BEFORE_TX, default=True): cv.boolean,
-    },
+        cv.Optional(CONF_ENROLLING): binary_sensor.binary_sensor_schema(
+            icon=ICON_KEY_PLUS,
+        ),
+        cv.Optional(CONF_STOP_MODE_UART): binary_sensor.binary_sensor_schema(
+            icon=ICON_CONFIG,
+        ),
+        cv.Optional(CONF_SET_STATUS_AT_BOOT): binary_sensor.binary_sensor_schema(
+            icon=ICON_CONFIG,
+        ),
+        cv.Optional(CONF_UART_IRQ_BEFORE_TX): binary_sensor.binary_sensor_schema(
+            icon=ICON_CONFIG,
+        ),
+    }
 )
 
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_FINGERPRINT_FPC2532_ID])
 
-    # This is the user-defined binary sensor from YAML
-    main_sensor = await binary_sensor.new_binary_sensor(config)
-    cg.add(hub.set_enrolling_binary_sensor(main_sensor))
-
-    # ---- Internal config-related binary sensors ----
-    # These are NOT in YAML -> must use BinarySensor.new()
-
-    set_status_sensor = binary_sensor.BinarySensor.new()
-    cg.add(set_status_sensor.set_icon(ICON_CONFIG))
-    cg.add(hub.set_status_at_boot_sensor(set_status_sensor))
-
-    stop_mode_sensor = binary_sensor.BinarySensor.new()
-    cg.add(stop_mode_sensor.set_icon(ICON_CONFIG))
-    cg.add(hub.set_stop_mode_uart_sensor(stop_mode_sensor))
-
-    uart_irq_sensor = binary_sensor.BinarySensor.new()
-    cg.add(uart_irq_sensor.set_icon(ICON_CONFIG))
-    cg.add(hub.set_uart_irq_before_tx_sensor(uart_irq_sensor))
+    for key in [
+        CONF_ENROLLING,
+        CONF_SET_STATUS_AT_BOOT,
+        CONF_STOP_MODE_UART,
+        CONF_UART_IRQ_BEFORE_TX,
+    ]:
+        if key not in config:
+            continue
+        conf = config[key]
+        sens = await binary_sensor.new_binary_sensor(conf)
+        cg.add(getattr(hub, f"set_{key}_sensor")(sens))
--- esphome/components/fingerprint_FPC2532/text_sensor.py
+++ esphome/components/fingerprint_FPC2532/text_sensor.py
@@ -1,14 +1,13 @@
 import esphome.codegen as cg
 from esphome.components import text_sensor
 import esphome.config_validation as cv
-from esphome.const import ENTITY_CATEGORY_DIAGNOSTIC
 
 from . import CONF_FINGERPRINT_FPC2532_ID, FingerprintFPC2532Component
 
-CONF_STATUS_TEXT = "text_status"
-CONF_UNIQUE_ID = "unique_id"
-CONF_VERSION = "version"
-ICON_CONFIG = "mdi:settings"
+# CONF_STATUS_TEXT = "text_status"
+# CONF_UNIQUE_ID = "unique_id"
+# CONF_VERSION = "version"
+# ICON_CONFIG = "mdi:settings"
 
 DEPENDENCIES = ["fingerprint_FPC2532"]
 
@@ -17,25 +16,11 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(CONF_FINGERPRINT_FPC2532_ID): cv.use_id(
             FingerprintFPC2532Component
         ),
-        cv.Optional(CONF_STATUS_TEXT): text_sensor.text_sensor_schema(
-            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-        ),
-        cv.Optional(CONF_UNIQUE_ID): text_sensor.text_sensor_schema(
-            # entity_category=ENTITY_CATEGORY_CONFIG,
-        ),
-        cv.Optional(CONF_VERSION): text_sensor.text_sensor_schema(
-            # entity_category=ENTITY_CATEGORY_CONFIG,
-        ),
     }
 )
 
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_FINGERPRINT_FPC2532_ID])
-
-    for key in [CONF_STATUS_TEXT, CONF_VERSION, CONF_UNIQUE_ID]:
-        if key not in config:
-            continue
-        conf = config[key]
-        sens = await text_sensor.new_text_sensor(conf)
-        cg.add(getattr(hub, f"set_{key}_sensor")(sens))
+    sens = await text_sensor.new_text_sensor(config)
+    cg.add(getattr(hub, f"set_{config.id}_sensor")(sens))
