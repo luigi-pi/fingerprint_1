a8a102fedefd8434e9da2a69361775f70f158f53
luigi-pi <pezzatoluigi.mail@gmail.com>
2025-11-17 23:15:56 +0100

    d

--- esphome/components/fingerprint_FPC2532/switch.py
+++ esphome/components/fingerprint_FPC2532/switch.py
@@ -5,6 +5,74 @@ from esphome.const import ENTITY_CATEGORY_CONFIG
 
 from . import CONF_FINGERPRINT_FPC2532_ID, FingerprintFPC2532Component
 
+# Switch constants
+CONF_SET_STATUS_AT_BOOT = "status_at_boot"
+CONF_STOP_MODE_UART = "stop_mode_uart"
+CONF_UART_IRQ_BEFORE_TX = "uart_irq_before_tx"
+ICON_CONFIG = "mdi:cog"
+
+DEPENDENCIES = ["fingerprint_FPC2532"]
+
+CONFIG_SCHEMA = cv.Schema(
+    {cv.GenerateID(CONF_FINGERPRINT_FPC2532_ID): cv.use_id(FingerprintFPC2532Component)}
+)
+
+
+async def to_code(config):
+    hub = await cg.get_variable(config[CONF_FINGERPRINT_FPC2532_ID])
+
+    # -------------------------------
+    # SWITCH 1: Status at Boot
+    # -------------------------------
+    sw1_conf = switch.SWITCH_SCHEMA(
+        {
+            cg.GenerateID(): cg.declare_id(switch.Switch),
+            "name": "Set Status At Boot",
+        }
+    )
+    sw1 = await switch.new_switch(sw1_conf)
+    cg.add(sw1.set_entity_category(ENTITY_CATEGORY_CONFIG))
+    cg.add(sw1.set_icon(ICON_CONFIG))
+    cg.add(hub.set_status_at_boot_switch(sw1))
+
+    # -------------------------------
+    # SWITCH 2: Stop Mode UART
+    # -------------------------------
+    sw2_conf = switch.SWITCH_SCHEMA(
+        {
+            cg.GenerateID(): cg.declare_id(switch.Switch),
+            "name": "Stop Mode UART",
+        }
+    )
+    sw2 = await switch.new_switch(sw2_conf)
+    cg.add(sw2.set_entity_category(ENTITY_CATEGORY_CONFIG))
+    cg.add(sw2.set_icon(ICON_CONFIG))
+    cg.add(hub.set_stop_mode_uart_switch(sw2))
+
+    # -------------------------------
+    # SWITCH 3: UART IRQ Before TX
+    # -------------------------------
+    sw3_conf = switch.SWITCH_SCHEMA(
+        {
+            cg.GenerateID(): cg.declare_id(switch.Switch),
+            "name": "UART IRQ Before TX",
+        }
+    )
+    sw3 = await switch.new_switch(sw3_conf)
+    cg.add(sw3.set_entity_category(ENTITY_CATEGORY_CONFIG))
+    cg.add(sw3.set_icon(ICON_CONFIG))
+    cg.add(hub.set_uart_irq_before_tx_switch(sw3))
+
+
+"""
+
+import esphome.codegen as cg
+from esphome.components import switch
+import esphome.config_validation as cv
+from esphome.const import ENTITY_CATEGORY_CONFIG
+
+from . import CONF_FINGERPRINT_FPC2532_ID, FingerprintFPC2532Component
+
 CONF_SET_STATUS_AT_BOOT = "status_at_boot"
 CONF_STOP_MODE_UART = "stop_mode_uart"
 CONF_UART_IRQ_BEFORE_TX = "uart_irq_before_tx"
@@ -51,3 +119,4 @@ async def to_code(config):
     cg.add(sw3.set_entity_category(ENTITY_CATEGORY_CONFIG))
     cg.add(sw3.set_icon(ICON_CONFIG))
     cg.add(hub.set_uart_irq_before_tx_switch(sw3))
+"""
