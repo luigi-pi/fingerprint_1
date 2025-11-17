28d55ec7b4564032603f2debd7b84ae2e8449826
luigi-pi <pezzatoluigi.mail@gmail.com>
2025-11-18 00:21:58 +0100

    c

--- esphome/components/fingerprint_FPC2532/switch.py
+++ esphome/components/fingerprint_FPC2532/switch.py
@@ -1,39 +1,55 @@
 import esphome.codegen as cg
 from esphome.components import switch
 import esphome.config_validation as cv
+from esphome.const import ENTITY_CATEGORY_CONFIG
 
 from . import CONF_FINGERPRINT_FPC2532_ID, FingerprintFPC2532Component
-
-CONF_SET_STATUS_AT_BOOT = "status_at_boot"
-CONF_STOP_MODE_UART = "stop_mode_uart"
-CONF_UART_IRQ_BEFORE_TX = "uart_irq_before_tx"
-ICON_CONFIG = "mdi:cog"
 
 DEPENDENCIES = ["fingerprint_FPC2532"]
 
 CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_FINGERPRINT_FPC2532_ID): cv.use_id(
-            FingerprintFPC2532Component
-        ),
-        cv.Optional(CONF_SET_STATUS_AT_BOOT): switch.switch_schema(),
-        cv.Optional(CONF_STOP_MODE_UART): switch.switch_schema(),
-        cv.Optional(CONF_UART_IRQ_BEFORE_TX): switch.switch_schema(),
-    }
+    {cv.GenerateID(CONF_FINGERPRINT_FPC2532_ID): cv.use_id(FingerprintFPC2532Component)}
 )
 
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_FINGERPRINT_FPC2532_ID])
 
-    if CONF_SET_STATUS_AT_BOOT in config:
-        sw1 = await switch.new_switch(config[CONF_SET_STATUS_AT_BOOT])
-        cg.add(hub.set_status_at_boot_switch(sw1))
+    #
+    # SWITCH 1
+    #
+    sw1 = await switch.new_switch(
+        {
+            cg.CONF_ID: cg.declare_id(switch.Switch),
+            "name": "Set Status At Boot",
+        }
+    )
+    await cg.register_component(sw1, {})  # <-- REQUIRED
+    cg.add(sw1.set_entity_category(ENTITY_CATEGORY_CONFIG))
+    cg.add(hub.set_status_at_boot_switch(sw1))
 
-    if CONF_STOP_MODE_UART in config:
-        sw2 = await switch.new_switch(config[CONF_STOP_MODE_UART])
-        cg.add(hub.set_stop_mode_uart_switch(sw2))
+    #
+    # SWITCH 2
+    #
+    sw2 = await switch.new_switch(
+        {
+            cg.CONF_ID: cg.declare_id(switch.Switch),
+            "name": "Stop Mode UART",
+        }
+    )
+    await cg.register_component(sw2, {})  # <-- REQUIRED
+    cg.add(sw2.set_entity_category(ENTITY_CATEGORY_CONFIG))
+    cg.add(hub.set_stop_mode_uart_switch(sw2))
 
-    if CONF_UART_IRQ_BEFORE_TX in config:
-        sw3 = await switch.new_switch(config[CONF_UART_IRQ_BEFORE_TX])
-        cg.add(hub.set_uart_irq_before_tx_switch(sw3))
+    #
+    # SWITCH 3
+    #
+    sw3 = await switch.new_switch(
+        {
+            cg.CONF_ID: cg.declare_id(switch.Switch),
+            "name": "UART IRQ Before TX",
+        }
+    )
+    await cg.register_component(sw3, {})  # <-- REQUIRED
+    cg.add(sw3.set_entity_category(ENTITY_CATEGORY_CONFIG))
+    cg.add(hub.set_uart_irq_before_tx_switch(sw3))
