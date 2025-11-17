4b766c4f4eeba40064bb78690d377f196006ee36
luigi-pi <pezzatoluigi.mail@gmail.com>
2025-11-18 00:27:18 +0100

    s

--- esphome/components/fingerprint_FPC2532/switch.py
+++ esphome/components/fingerprint_FPC2532/switch.py
@@ -5,6 +5,11 @@ from esphome.const import ENTITY_CATEGORY_CONFIG
 
 from . import CONF_FINGERPRINT_FPC2532_ID, FingerprintFPC2532Component
 
+CONF_SET_STATUS_AT_BOOT = "status_at_boot"
+CONF_STOP_MODE_UART = "stop_mode_uart"
+CONF_UART_IRQ_BEFORE_TX = "uart_irq_before_tx"
+ICON_CONFIG = "mdi:cog"
+
 DEPENDENCIES = ["fingerprint_FPC2532"]
 
 CONFIG_SCHEMA = cv.Schema(
@@ -15,41 +20,37 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_FINGERPRINT_FPC2532_ID])
 
-    #
+    await cg.register_component(hub, config)
+
     # SWITCH 1
-    #
     sw1 = await switch.new_switch(
         {
-            cg.CONF_ID: cg.declare_id(switch.Switch),
+            cg.GenerateID(): cg.declare_id(switch.Switch),
             "name": "Set Status At Boot",
         }
     )
-    await cg.register_component(sw1, {})  # <-- REQUIRED
     cg.add(sw1.set_entity_category(ENTITY_CATEGORY_CONFIG))
+    cg.add(sw1.set_icon(ICON_CONFIG))
     cg.add(hub.set_status_at_boot_switch(sw1))
 
-    #
     # SWITCH 2
-    #
     sw2 = await switch.new_switch(
         {
-            cg.CONF_ID: cg.declare_id(switch.Switch),
+            cg.GenerateID(): cg.declare_id(switch.Switch),
             "name": "Stop Mode UART",
         }
     )
-    await cg.register_component(sw2, {})  # <-- REQUIRED
     cg.add(sw2.set_entity_category(ENTITY_CATEGORY_CONFIG))
+    cg.add(sw2.set_icon(ICON_CONFIG))
     cg.add(hub.set_stop_mode_uart_switch(sw2))
 
-    #
     # SWITCH 3
-    #
     sw3 = await switch.new_switch(
         {
-            cg.CONF_ID: cg.declare_id(switch.Switch),
+            cg.GenerateID(): cg.declare_id(switch.Switch),
             "name": "UART IRQ Before TX",
         }
     )
-    await cg.register_component(sw3, {})  # <-- REQUIRED
     cg.add(sw3.set_entity_category(ENTITY_CATEGORY_CONFIG))
+    cg.add(sw3.set_icon(ICON_CONFIG))
     cg.add(hub.set_uart_irq_before_tx_switch(sw3))
