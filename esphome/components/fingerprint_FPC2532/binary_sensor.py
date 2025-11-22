d72c5770cf23887f1d911d02992a6afce6de8a35
luigi-pi <196961282+luigi-pi@users.noreply.github.com>
2025-11-22 11:02:50 +0100

    d

--- esphome/components/fingerprint_FPC2532/binary_sensor.py
+++ esphome/components/fingerprint_FPC2532/binary_sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 from esphome.components import binary_sensor
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import ENTITY_CATEGORY_DIAGNOSTIC
 
 from . import CONF_FINGERPRINT_FPC2532_ID, FingerprintFPC2532Component
 
@@ -9,37 +9,32 @@ CONF_SET_STATUS_AT_BOOT = "status_at_boot"
 CONF_STOP_MODE_UART = "stop_mode_uart"
 CONF_UART_IRQ_BEFORE_TX = "uart_irq_before_tx"
 CONF_ENROLLING = "enrolling_binary"
-ICON_CONFIG = "mdi:cog"
 
+ICON_CONFIG = "mdi:cog"
+ICON_ENROLL = "mdi:key-plus"
 
 DEPENDENCIES = ["fingerprint_FPC2532"]
 
-"""
-def validate_icons(config):
-    sensor_id = str(config[CONF_ID])
-    config_icon_group = {
-        CONF_SET_STATUS_AT_BOOT,
-        CONF_STOP_MODE_UART,
-        CONF_UART_IRQ_BEFORE_TX,
-    }
-    if sensor_id in config_icon_group:
-        icon = ICON_CONFIG
-    elif sensor_id == CONF_ENROLLING:
-        icon = ICON_KEY_PLUS
-    else:
-        icon = "mdi:checkbox-blank-outline"
-    return icon
-"""
-CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend(
+CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_FINGERPRINT_FPC2532_ID): cv.use_id(
             FingerprintFPC2532Component
         ),
-        cv.Required(CONF_ID): cv.declare_id(binary_sensor.BinarySensor),
-        cv.Optional(CONF_ENROLLING): cv.boolean,
-        cv.Optional(CONF_UART_IRQ_BEFORE_TX): cv.boolean,
-        cv.Optional(CONF_SET_STATUS_AT_BOOT): cv.boolean,
-        cv.Optional(CONF_STOP_MODE_UART): cv.boolean,
+        cv.Optional(CONF_ENROLLING): binary_sensor.binary_sensor_schema(
+            icon=ICON_ENROLL,
+        ),
+        cv.Optional(CONF_UART_IRQ_BEFORE_TX): binary_sensor.binary_sensor_schema(
+            icon=ICON_CONFIG,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        ),
+        cv.Optional(CONF_SET_STATUS_AT_BOOT): binary_sensor.binary_sensor_schema(
+            icon=ICON_CONFIG,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        ),
+        cv.Optional(CONF_STOP_MODE_UART): binary_sensor.binary_sensor_schema(
+            icon=ICON_CONFIG,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        ),
     }
 )
 
@@ -47,24 +42,18 @@ CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend(
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_FINGERPRINT_FPC2532_ID])
 
-    sens = await binary_sensor.new_binary_sensor(config)
-
     if CONF_ENROLLING in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_ENROLLING])
         cg.add(hub.set_enrolling_binary_sensor(sens))
 
     if CONF_UART_IRQ_BEFORE_TX in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_UART_IRQ_BEFORE_TX])
         cg.add(hub.set_uart_irq_before_tx_sensor(sens))
 
     if CONF_SET_STATUS_AT_BOOT in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_SET_STATUS_AT_BOOT])
         cg.add(hub.set_status_at_boot_sensor(sens))
 
     if CONF_STOP_MODE_UART in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_STOP_MODE_UART])
         cg.add(hub.set_stop_mode_uart_sensor(sens))
-
-
-"""
-    if CONF_ICON not in config:
-        icon = validate_icons(config)
-        cg.add(sens.set_icon(icon))
-    cg.add(getattr(hub, f"set_{config[CONF_ID]}_sensor")(sens))
-"""
